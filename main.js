d2a84f10f3d559bcd1aa08f9c4a7e00ce7273aa8
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-02-24 16:12:20 +0100

    finished

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-import './style.css'
+import "./style.css";
+import Chance from "chance";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const randomAvatar = chance.avatar();
+const randomName = chance.name();
+const randomEmail = chance.email();
+const randomPhoneNumber = chance.phone();
+const randomProfession = chance.profession();
+const randomYear = chance.year();
+const randomCity = chance.city();
+const randomAnimal = chance.animal();
+const randomNumber = chance.integer();
+chance.integer({ min: 1, max: 1000 });
+
+document.querySelector("#app").innerHTML = `
+  <img src="${randomAvatar}"/>
+  <h1>${randomName}</h1>
+  <p>${randomEmail}</p>
+  <p>${randomPhoneNumber}</p>
+  <article> Hey, my Name is ${randomName} and I am a <br> ${randomProfession}. I was born in <br> the year of ${randomYear} in the city of ${randomCity}. <br> My favorite animal is the ${randomAnimal}. But I also love <br> numbers! My favorite one is ${randomNumber}.
+`;
